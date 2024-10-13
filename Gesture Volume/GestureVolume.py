51f49c818282b54dee09b4ad20281a21c334659a
purohit007 <KESHAVMAHADEV22@GMAIL.COM>
2024-10-13 16:05:34 +0530

    some change

--- Gesture Volume/GestureVolume.py
+++ Gesture Volume/GestureVolume.py
@@ -1,7 +1,7 @@
 import cv2
 import time
 import numpy as np
-import HandTrackingModule as htm  # Ensure this module is correctly implemented
+import HandTrackingModule as html  # Ensure this module is correctly implemented
 import math
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
