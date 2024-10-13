d785f6ebfe931c325976fbf93c0d9d3b0f4d81e0
purohit007 <KESHAVMAHADEV22@GMAIL.COM>
2024-10-13 15:59:35 +0530

    solve import error from opencv2

--- Gesture Volume/GestureVolume.py
+++ Gesture Volume/GestureVolume.py
@@ -1,7 +1,7 @@
 import cv2
 import time
 import numpy as np
-import HandTrackingModule as htm
+import HandTrackingModule as htm  # Ensure this module is correctly implemented
 import math
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
@@ -12,18 +12,20 @@ wCam, hCam = 640, 480
 ################################
 
 cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error: Could not open webcam.")
+    exit()
+
 cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7, maxHands=1)
 
+# Set up volume control
 devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
-# volume.GetMute()
-# volume.GetMasterVolumeLevel()
 volRange = volume.GetVolumeRange()
 minVol = volRange[0]
 maxVol = volRange[1]
@@ -35,20 +37,19 @@ colorVol = (255, 0, 0)
 
 while True:
     success, img = cap.read()
+    if not success:
+        print("Error: Could not read frame from webcam.")
+        break
 
     # Find Hand
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img, draw=True)
     if len(lmList) != 0:
-
         # Filter based on size
         area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]) // 100
-        # print(area)
         if 250 < area < 1000:
-
             # Find Distance between index and Thumb
             length, img, lineInfo = detector.findDistance(4, 8, img)
-            # print(length)
 
             # Convert Volume
             volBar = np.interp(length, [50, 200], [400, 150])
@@ -60,7 +61,6 @@ while True:
 
             # Check fingers up
             fingers = detector.fingersUp()
-            # print(fingers)
 
             # If pinky is down set volume
             if not fingers[4]:
@@ -73,18 +73,21 @@ while True:
     # Drawings
     cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
     cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
-    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
-                1, (255, 0, 0), 3)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
     cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
-    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX,
-                1, colorVol, 3)
+    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX, 1, colorVol, 3)
 
     # Frame rate
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
-                1, (255, 0, 0), 3)
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
 
     cv2.imshow("Img", img)
-    cv2.waitKey(1)
+
+    # Exit on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
